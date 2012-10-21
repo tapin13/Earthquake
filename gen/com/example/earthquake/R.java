135a805316d3af440b37462a3ca7ebd109aeb15c
tapin13 <tapin13@gmail.com>
2012-10-21 18:22:15 +0200

    add prefs

--- gen/com/example/earthquake/R.java
+++ gen/com/example/earthquake/R.java
@@ -8,38 +8,54 @@
 package com.example.earthquake;
 
 public final class R {
+    public static final class array {
+        public static final int magnitude=0x7f040001;
+        public static final int magnitude_options=0x7f040002;
+        public static final int update_freq_options=0x7f040000;
+        public static final int update_freq_values=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int padding_large=0x7f040002;
-        public static final int padding_medium=0x7f040001;
-        public static final int padding_small=0x7f040000;
+        public static final int padding_large=0x7f050002;
+        public static final int padding_medium=0x7f050001;
+        public static final int padding_small=0x7f050000;
     }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int earthquakeListView=0x7f080000;
-        public static final int menu_settings=0x7f080002;
-        public static final int quakeDetailsTextView=0x7f080001;
+        public static final int cancelButton=0x7f090005;
+        public static final int checkbox_auto_update=0x7f090001;
+        public static final int earthquakeListView=0x7f090000;
+        public static final int menu_settings=0x7f090007;
+        public static final int okButton=0x7f090004;
+        public static final int quakeDetailsTextView=0x7f090006;
+        public static final int spinner_quake_mag=0x7f090003;
+        public static final int spinner_update_freq=0x7f090002;
     }
     public static final class layout {
         public static final int activity_earthquake=0x7f030000;
-        public static final int quake_details=0x7f030001;
+        public static final int preferences=0x7f030001;
+        public static final int quake_details=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_earthquake=0x7f070000;
+        public static final int activity_earthquake=0x7f080000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int menu_settings=0x7f050002;
-        public static final int menu_update=0x7f050005;
-        public static final int quake_feed=0x7f050004;
-        public static final int title_activity_earthquake=0x7f050003;
+        public static final int app_name=0x7f060000;
+        public static final int auto_update_prompt=0x7f060006;
+        public static final int hello_world=0x7f060001;
+        public static final int menu_preferences=0x7f060009;
+        public static final int menu_settings=0x7f060002;
+        public static final int menu_update=0x7f060005;
+        public static final int min_quake_mag_prompt=0x7f060008;
+        public static final int quake_feed=0x7f060004;
+        public static final int title_activity_earthquake=0x7f060003;
+        public static final int update_freq_prompt=0x7f060007;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
     }
 }
